b52e900cd8c17f40040a3ba4a4a90a6a5c02d6b6
Chaviland7 <charliehaviland@gmail.com>
2019-03-15 07:30:01 -0400

    adding funky fade functionality

--- js/script.js
+++ js/script.js
@@ -64,6 +64,37 @@ function cycleCaptions(){
     now.fadeOut(speed);
     next.fadeIn(speed);
 }
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + ($(this).outerHeight() / 2);
+
+      /* If the element is halfway within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      }
+    });
+    $(".skill_bar").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {
+          $(this).fadeTo(500,1);
+          $(this).animate({width: $(this).data('width')})
+        }
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {
+          $(this).fadeTo(500,0);
+          $(this).animate({width: '15%'})
+        }
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
 $(document).ready(function() {
   positionStuff();
   get_tenures();
